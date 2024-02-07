9a09fbfffadc77cf16b74f25b0f53bcb99c9d71f
redaelhoumadi <reda.elhoumadi@gmail.com>
2024-02-07 13:32:30 +0100

    Tri par date des notes de frais

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -28,8 +28,10 @@ describe("Given I am connected as an employee", () => {
       //to-do write expect expression
 
     })
+    // Vérifie le tri par date des notes de frais
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
+      /* Modifier le test pour qu'ils prennent en compte les bills comment ils sont rangés dans la vue */
+      document.body.innerHTML = BillsUI({ data: bills.sort((a, b) => (a.date < b.date) ? 1 : -1) })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
